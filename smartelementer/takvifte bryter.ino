e82d52e8fceadd7e11d1a2bd06972e1ebf69aa4a
lundi97 <81311139+lundi97@users.noreply.github.com>
2021-04-15 12:12:56 +0100

    Update takvifte bryter.ino

--- smartelementer/takvifte bryter.ino
+++ smartelementer/takvifte bryter.ino
@@ -1,25 +1,60 @@
-const int buttonPin = 2;     
+#include <analogWrite.h>
 
-int buttonState = 0;         
-int counter = 0;
-int prestate = 0;
+enum States {ceiling_fan,
+ ceiling_fan_ON,
+ ceiling_fan_OFF};
+enum States state;
 
-void setup() {
-  pinMode(buttonPin, INPUT);
-  Serial.begin(9600);
+const int buttonPin = 15;
+
+int count = 0;
+int ON = 1;
+int OFF = 0;
+int button_state = LOW;
+
+char ssid[] = "*********"; // Navn apå nettverk.
+char password[] = "**********"; // Passord til nettverk.
+char server[] = "www.circusofthings.com"; // Her ligger serveren.
+char ceiling_fan_key[] = "25316"; // Nøkkel for signalet på CoT.
+char token[] = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0ODkwIn0.HRqG5DABkLgWQoQzRgj8RrnLVtWLYP3o7c-Ycpksl7o"; // Presonlig nøkkel for kontoen.
+CircusESP32Lib circusESP32(server,ssid,password);// Her leses nettadressen til CoT, ssid, og passord inn.
+
+void setup()
+{
+circusESP32.begin();  
+state = ceiling_fan;
+pinMode(buttonPin, INPUT);
+Serial.begin(9600);
+
 }
-
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  Serial.println(counter);
-  if (buttonState == HIGH && prestate == 0){
-      counter = 1;
-      prestate = 1;
+switch (state) {
+ case ceiling_fan:
+      button_state = digitalRead(buttonPin);
+      if(button_state == HIGH) {
+        if (count == 0){
+          state = ceiling_fan_ON;
+        }
+        else{
+          state = ceiling_fan_OFF;
+        }
+      }
+      break;
+
+ case ceiling_fan_ON:
+      Serial.println("ON");
+      count = 1;
+      circusESP32.write(ceiling_fan_key, ON, token);
+      state = ceiling_fan;
       delay(1000);
-  }
-  else if (buttonState == HIGH && prestate == 1){
-    counter = 0;
-    prestate = 0;
-    delay(1000);
-    }
+      break;
+
+ case ceiling_fan_OFF:
+      Serial.println("OFF");
+      count = 0;
+      circusESP32.write(ceiling_fan_key, OFF, token);
+      state = ceiling_fan;
+      delay(1000);
+      break;
+}
 }
